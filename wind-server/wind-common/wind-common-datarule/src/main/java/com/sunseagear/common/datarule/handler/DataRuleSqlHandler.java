8ccd55187bfa90ad807ef5de3687beddd573fe0c
dothegod <dothegod@gmail.com>
2021-10-02 14:13:01 +0800

    数据权限

--- wind-server/wind-common/wind-common-datarule/src/main/java/com/sunseagear/common/datarule/handler/DataRuleSqlHandler.java
+++ wind-server/wind-common/wind-common-datarule/src/main/java/com/sunseagear/common/datarule/handler/DataRuleSqlHandler.java
@@ -1,10 +1,19 @@
 package com.sunseagear.common.datarule.handler;
 
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLLimit;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.statement.SQLSelectQuery;
+import com.alibaba.druid.sql.ast.statement.SQLSelectQueryBlock;
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
+import com.alibaba.druid.util.JdbcConstants;
 import com.sunseagear.common.datarule.model.DataRuleModel;
 import com.sunseagear.common.utils.StringUtils;
 import com.sunseagear.common.utils.entity.Principal;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.Template;
@@ -12,7 +21,10 @@ import org.beetl.core.resource.StringTemplateResourceLoader;
 import org.beetl.core.statement.ErrorGrammarProgram;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +36,9 @@ public class DataRuleSqlHandler {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
     private static GroupTemplate groupTemplate;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driverClassName;
 
     static {
         StringTemplateResourceLoader resourceLoader = new StringTemplateResourceLoader();
@@ -39,36 +54,54 @@ public class DataRuleSqlHandler {
 
     private final DataRuleHandler dataRuleHandler;
 
+    private String getDataSourceType() {
+        if (driverClassName.contains("mysql")) {
+            return JdbcConstants.MYSQL;
+        } else if (driverClassName.contains("sqlserver")) {
+            return JdbcConstants.SQL_SERVER;
+        } else if (driverClassName.contains("oracle")) {
+            return JdbcConstants.ORACLE;
+        } else {
+            return JdbcConstants.MYSQL;
+        }
+    }
+
     public String sqlCondition(DataRuleModel dataScope, Principal principal, String originalSql) {
 
         //判断数据权限类型并组装对应Sql
         Integer scopeRule = Objects.requireNonNull(dataScope).getScopeType();
-        String id = "";
-        Map<String, Object> userMap = dataRuleHandler.getUser(principal.getId());
         if (DataRuleModel.ALL == scopeRule) {
             return null;
-        } else if (DataRuleModel.CUSTOM == scopeRule) {
+        }
+        Map<String, Object> userMap = dataRuleHandler.getUser(principal.getId());
+        SQLSelectStatement selectStmt = (SQLSelectStatement) SQLUtils.parseSingleStatement(originalSql, getDataSourceType());
+        SQLSelectQueryBlock mySqlSelectQueryBlock = (SQLSelectQueryBlock) selectStmt.getSelect().getQuery();
+        SQLLimit limit = mySqlSelectQueryBlock.getLimit();
+        mySqlSelectQueryBlock.setLimit(null);
+        String tempSql = selectStmt.toString();
+        String id = "";
+        String result = "";
+        if (DataRuleModel.CUSTOM == scopeRule) {
             Template template = groupTemplate.getTemplate(StringEscapeUtils.unescapeHtml4(dataScope.getScopeValue()));
-            if (template.program instanceof ErrorGrammarProgram){
-                logger.info(((ErrorGrammarProgram)template.program).getException().detailCode);
+            if (template.program instanceof ErrorGrammarProgram) {
+                logger.info(((ErrorGrammarProgram) template.program).getException().detailCode);
             }
             userMap.forEach(template::binding);
-            String whereSql =  template.render();
-            return String.format(" select %s from (%s) scope " + whereSql, dataScope.getScopeField(), originalSql);
+            String whereSql = template.render();
+            result = String.format(" select %s from (%s) scope " + whereSql, dataScope.getScopeField(), tempSql);
         } else if (DataRuleModel.OWN == scopeRule) {
             String whereSql = "where scope.%s = '%s'";
             id = principal.getId();
-            return String.format(" select %s from (%s) scope " + whereSql, dataScope.getScopeField(), originalSql, dataScope.getScopeColumn(), id);
-
+            result = String.format(" select %s from (%s) scope " + whereSql, dataScope.getScopeField(), tempSql, dataScope.getScopeColumn(), id);
         } else if (DataRuleModel.OWN_ORG == scopeRule) {
             id = (String) userMap.get(dataScope.getUserEntityField());
             if (StringUtils.isEmpty(id)) {
                 logger.error(String.format("DataRuleSqlHandler OWN_ORG error id is not exist userId:%s,UserColumn：%s", principal.getId(), dataScope.getUserEntityField()));
                 return null;
             }
-            return String.format("select %s from (%s) scope, %s a, sys_user sysu where a.id = sysu.%s" +
+            result = String.format("select %s from (%s) scope, %s a, sys_user sysu where a.id = sysu.%s" +
                             " and sysu.id = scope.%s and a.id = '%s'",
-                    dataScope.getScopeField(), originalSql,
+                    dataScope.getScopeField(), tempSql,
                     dataScope.getTableName(),
                     dataScope.getUserColumn(), dataScope.getScopeColumn(),
                     id);
@@ -83,14 +116,18 @@ public class DataRuleSqlHandler {
             List<String> deptIdList = dataRuleHandler.getChildren(dataScope.getTableName(), id);
             ids.addAll(deptIdList);
             id = StringUtils.join(ids, "','");
-            return String.format("select %s from (%s) scope, %s a, sys_user sysu where a.id = sysu.%s" +
+            result = String.format("select %s from (%s) scope, %s a, sys_user sysu where a.id = sysu.%s" +
                             " and sysu.id = scope.%s and a.id in ('%s')",
-                    dataScope.getScopeField(), originalSql,
+                    dataScope.getScopeField(), tempSql,
                     dataScope.getTableName(),
                     dataScope.getUserColumn(), dataScope.getScopeColumn(),
                     id);
         }
-        return null;
+        SQLSelectStatement selectStmtResult = (SQLSelectStatement) SQLUtils.parseSingleStatement(result, getDataSourceType());
+        SQLSelectQueryBlock mySqlSelectQueryBlockResult = (SQLSelectQueryBlock) selectStmtResult.getSelect().getQuery();
+        mySqlSelectQueryBlockResult.setLimit(limit);
+        logger.info(String.format("result sql: %s", selectStmtResult));
+        return selectStmtResult.toString();
     }
 
 }
