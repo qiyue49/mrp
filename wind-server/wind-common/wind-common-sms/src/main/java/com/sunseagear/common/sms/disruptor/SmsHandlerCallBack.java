19c13a0ebf192f3b8d0d4ee623950fccff3d2dbd
zhangyp <zhangyp@dragonpass.com.cn>
2026-04-04 18:21:59 +0800

    chore: clean repo, remove unrelated wind-server dir

--- wind-server/wind-common/wind-common-sms/src/main/java/com/sunseagear/common/sms/disruptor/SmsHandlerCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.sunseagear.common.sms.disruptor;
-
-import com.sunseagear.common.sms.data.SmsResult;
-
-import java.io.Serializable;
-
-public interface SmsHandlerCallBack extends Serializable {
-    void onResult(SmsResult smsResult);
-}
